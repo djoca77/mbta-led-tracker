3b327c33ee084ed8ebc8b9ce2032f7427c725377
Danny Joca <danny-joca@djwork.fios-router.home>
2025-03-24 23:56:10 -0400

    streamlined API calls, only having to call twice per line, 8 times per iteration for all lines

--- mbta_api.py
+++ mbta_api.py
@@ -8,26 +8,28 @@ import time
 import stops
 import config
 import constants
+import sys
 
 API_KEY = config.key
 
 lineFlag = {"Orange":"o", "Blue":"b", "Red":"r", "Green":"g"}
 lines = ["Blue", "Orange", "Red"]
 
-def populate_bits(line, stops, arduino):
-    URL = "https://api-v3.mbta.com/vehicles?filter[route]=" + line + "&api_key=" + API_KEY
-    vehicles = requests.get(url=URL).json()['data']
-
+def populate_bits(line, stops, arduino, direction):
     out_bits = [0] * len(stops)
+    
+    VEHICLES_URL = "https://api-v3.mbta.com/vehicles?filter[route]=" + line + "&filter[direction_id]=" + direction
+    vehicles = requests.get(url=VEHICLES_URL).json()['data']
 
-    for vehicle in vehicles:
-        if vehicle["attributes"]["direction_id"] == constants.NORTHBOUND:
-            stop_id = str(vehicle["relationships"]["stop"]["data"]["id"])
+    stop_ids = [str(v["relationships"]["stop"]["data"]["id"]) for v in vehicles]
 
-            STOPS_URL = f"https://api-v3.mbta.com/stops/{stop_id}"
-            stop_name = requests.get(url=STOPS_URL).json()['data']['attributes']['name']
+    STOPS_URL = f"https://api-v3.mbta.com/stops?filter[id]={','.join(stop_ids)}"
+    stops_data = requests.get(url=STOPS_URL).json()
 
-            out_bits[(stops[stop_name])] = 1
+    for i in range(len(vehicles)):
+        stop_name = stops_data['data'][i]['attributes']['name']
+
+        out_bits[(stops[stop_name])] = 1
 
     binary_string = ''.join(map(str, out_bits))
     output = lineFlag[line] + binary_string + '\n'
@@ -35,24 +37,42 @@ def populate_bits(line, stops, arduino):
 
     # Send the bytes serially
     arduino.write(output.encode())
-            
 
-if __name__ == "__main__":
+def main(direction):
     arduino = serial.Serial('/dev/ttyACM0', '9600')
     time.sleep(2)
 
     if not os.path.exists('stops.txt'):
         stops.populate_stop_dict()
 
-    while 1:
+    while True:
         stop_dicts = open('stops.txt', 'r')
 
         for i, line in enumerate(stop_dicts):
             stop_dict = json.loads(line)
-            populate_bits(lines[i], stop_dict, arduino)
+            populate_bits(lines[i], stop_dict, arduino, direction)
+            time.sleep(1)
 
         stop_dicts.close()
 
         time.sleep(30)
+
+def parse_dir(dir):
+    if dir.lower() == "n":
+        return constants.NORTHBOUND
+    elif dir.lower() == "s":
+        return constants.SOUTHBOUND
+    return -1
+
+
+if __name__ == "__main__":
+    arg_dir = sys.argv[1:]
+    direction = parse_dir(arg_dir[0])
+
+    if direction == -1:
+        print("Not a valid direction code, either input n for northbound or s for southbound trains")
+        exit(1)
+
+    main(direction)
 
     
